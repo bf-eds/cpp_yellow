6e8501b15343827d0b8c508a30cb6a3d16beb693
bf-eds <d.eroshenkov@dreamkas.ru>
2018-10-28 19:29:40 +0300

    Week :four: Ass. :eleven:

--- week_4/assignment_11/build_expression.cpp
+++ week_4/assignment_11/build_expression.cpp
@@ -7,43 +7,26 @@
 
 using namespace std;
 
-struct operation
-{
-    string name;
-    int precedence;
-};
-
 int main()
 {
-    queue<operation> expression;
+    queue<string> expression;
     int x = 0;
     cin >> x;
-    expression.push({to_string(x), 0});
+    expression.push(to_string(x));
 
     unsigned int num = 0;
     cin >> num;
     cin.get();
     for (unsigned int i = 0; i < num; ++i)
     {
-        operation line;
-        getline(cin, line.name);
-        char op = line.name.front();
-
-        if ((op == '*') || (op == '/'))
-        {
-            line.precedence = 2;
-        }
-        else
-        {
-            line.precedence = 1;
-        }
-
+        string line;
+        getline(cin, line);
         expression.push(line);
     }
 
     if (expression.size() == 1)
     {
-        cout << expression.front().name << endl;
+        cout << expression.front() << endl;
     }
     else
     {
--- week_4/assignment_11/build_expression_without_redundant_parentheses.cpp
+++ week_4/assignment_11/build_expression_without_redundant_parentheses.cpp
@@ -2,3 +2,77 @@
 // Created by human on 26.10.2018.
 //
 
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct operation
+{
+    string name;
+    int precedence = 0;
+};
+
+int main()
+{
+    queue<operation> expression;
+    int x = 0;
+    cin >> x;
+    expression.push({to_string(x), 3});
+
+    unsigned int num = 0;
+    cin >> num;
+    cin.get();
+    unsigned int parenthesesNum = 0;
+    for (unsigned int i = 0; i < num; ++i)
+    {
+        operation line;
+        getline(cin, line.name);
+        char op = line.name.front();
+
+        if ((op == '*') || (op == '/'))
+        {
+            line.precedence = 2;
+        }
+        else
+        {
+            line.precedence = 1;
+        }
+
+        operation prev = expression.back();
+
+        if (prev.precedence < line.precedence)
+        {
+            parenthesesNum++;
+        }
+
+        expression.push(line);
+    }
+
+    if (expression.size() == 1)
+    {
+        cout << expression.front().name << endl;
+    }
+    else
+    {
+        cout << string(parenthesesNum, '(');
+
+        while (!expression.empty())
+        {
+            operation str = expression.front();
+            expression.pop();
+            cout << str.name;
+
+            if (str.precedence < expression.front().precedence)
+            {
+                cout << ") ";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+    }
+
+    return 0;
+}
