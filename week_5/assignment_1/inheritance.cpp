ff8a158c349656ea19b8b04b935ea72ea7be6367
d.eroshenkov <d.eroshenkov@dreamkas.ru>
2018-11-05 13:15:19 +0300

    Week :five: Ass. :one:

---
+++ week_5/assignment_1/inheritance.cpp
@@ -0,0 +1,34 @@
+//
+// Created by d.eroshenkov on 01.11.2018.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Animal
+{
+public:
+    Animal(const string &s) : Name(s){}
+
+    const string Name;
+};
+
+
+class Dog : public Animal
+{
+public:
+    Dog(const string &s): Animal(s){}
+
+    void Bark()
+    {
+        cout << Name << " barks: woof!" << endl;
+    }
+};
+//int main()
+//{
+//    Dog g("Rob");
+//    g.Bark();
+//
+//    return 0;
+//}
